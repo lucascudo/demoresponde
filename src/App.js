436461d0da44fc589a97b5d897356f770215a3dc
Lucas Teixeira Rocha <lucasrochabr@outlook.com>
2018-03-21 15:03:08 -0300

    Add logos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import IllusionistForm from './components/IllusionistForm';
-import logo from './logo.svg';
 import './App.css';
+
+const logos = [
+    'bio_pentagram.png',
+    'pentagram-blue-2.png',
+    'BlackGreenPentagram.png',
+    'Pentagram.png',
+    'blood_splatter.png',
+    'pentagram_purple.png',
+    'logo.svg',
+    'pentagram_scare.png',
+    'metallic_pentagram.png',
+    'purplepentodd.gif',
+    'Pentacle_red.png',
+    'white_pentagram.png',
+    'Pentagram 2.png',
+];
+const logo = require('./logos/' + logos[Math.floor(Math.random() * logos.length)]);
+
 
 class App extends Component {
   render() {
@@ -9,14 +26,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Demo Responde</h1>
         </header>
         <br />
         <div>
+            <h1 className="App-title">Demo Responde</h1>
+            <IllusionistForm />
             <p>
                 * Dica: O tinhoso gosta de ser exaltado e não gosta de ficar de falatório, portanto capriche nos elogios e nada de perguntas que exijam uma "dissertação" como resposta, ou o coisa ruim pode resolver puxar seu pé a noite!
             </p>
-            <IllusionistForm />
         </div>
       </div>
     );
